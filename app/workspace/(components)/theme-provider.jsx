557bba2421aa678553997390515ffddb534f352f
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-02 19:24:55 +0530

    fix colors

--- app/workspace/(components)/theme-provider.jsx
+++ app/workspace/(components)/theme-provider.jsx
@@ -5,7 +5,19 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme);
+    document.documentElement.classList.add("dark");
+  }, [theme]);
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
