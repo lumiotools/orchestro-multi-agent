e95d507fc2efaf7a554974e0be3641df25260cbf
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-02 19:12:20 +0530

    update theme

--- app/workspace/(components)/theme-provider.jsx
+++ app/workspace/(components)/theme-provider.jsx
@@ -5,7 +5,7 @@ import React, { createContext, useState, useEffect } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState("light");
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
     const savedTheme = localStorage.getItem("theme");
