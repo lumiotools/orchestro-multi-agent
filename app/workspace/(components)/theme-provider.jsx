3f65b32c57b80ae3ab87b6227c30a7da82983594
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-02 19:14:08 +0530

    update

--- app/workspace/(components)/theme-provider.jsx
+++ app/workspace/(components)/theme-provider.jsx
@@ -7,18 +7,6 @@ export const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState("dark");
 
-  useEffect(() => {
-    const savedTheme = localStorage.getItem("theme");
-    if (savedTheme) {
-      setTheme("dark");
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    document.documentElement.classList.toggle("dark", theme === "dark");
-  }, [theme]);
-
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
       {children}
